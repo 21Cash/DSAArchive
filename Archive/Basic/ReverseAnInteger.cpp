a8e196b9b4a4d937bcc4f31be764096e82bf6fe6
21ThousandProof <21ThousandProof.@gmail.com>
2022-12-10 14:21:56 +0530

    Added ReverseAnInteger.cpp

---
+++ Archive/Basic/ReverseAnInteger.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef pair<int, int> pi;
+typedef long long ll;
+#define PI 3.141592653589793238462
+#define rep(i,a,b) for (int i = a; i < b; i++)
+#define PB push_back
+#define MP make_pair
+#define F first
+#define S second
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#ifndef ONLINE_JUDGE
+#define db(x) cout<<#x<<" : "<<x<<endl;
+#define debug(x) cerr<<#x<<" : "<<x<<endl;
+#define pvec(v) cout<<"[";rep(x, 0, v.size()){cout<<v[x];if(x<v.size()-1)cout<<" ";}cout<<"]"<<endl;
+#else
+#define pvec(v) ;rep(x, 0, v.size()){cout<<v[x];if(x<v.size()-1)cout<<" ";}cout<<endl;
+#define db(x)
+#define debug(x)
+#endif
+#define getVal(c) (c-'a'+1)
+
+const int N = 0;
+
+int GetReversed(int x) {
+	
+	if(x == 0) return x;
+	
+	
+	int reversedN = 0;
+	
+	while(x != 0) {
+		int pop = x % 10;
+		x /= 10;
+		reversedN = (reversedN * 10) + pop;
+	}
+	return reversedN;
+}
+
+
+
+
+void Solution(){
+	
+	int n;
+	cin >> n;
+	cout << GetReversed(n) << "\n";
+	
+}
+
+
+signed int main(){
+	
+    #ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    // freopen("error.txt", "w", stderr);
+    #endif
+	
+	ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+	
+	int t = 1;
+	//cin >> t;
+	while(t--){
+		Solution();
+	}
+	return 0;
+}
